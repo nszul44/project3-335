f5531bdb25d5bfdedcb187433dcb6203828315f2
nszul44 <136347369+nszul44@users.noreply.github.com>
2024-05-07 00:23:55 -0400

    QuickSelect1

---
+++ QuickSelect1.hpp
@@ -0,0 +1,22 @@
+/**
+ * @file QuickSelect1.hpp
+ * @author Nicholas Szul
+ * @brief interface for quickselect1
+ * @version 0.1
+ * @date 2024-05-06
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+#include <iostream>
+#include <vector>
+#include <string>
+#include <cmath>
+#include <iomanip>
+#include <fstream>
+#include <sstream>
+#include <algorithm>
+
+
+
+void quickSelect1 (const std::string & header, std::vector<int> data);

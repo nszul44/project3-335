3b04f2852846d127862f6dcf98e185468816f6b7
nszul44 <136347369+nszul44@users.noreply.github.com>
2024-05-10 04:59:41 -0400

    Update

---
+++ QuickSelect2.hpp
@@ -0,0 +1,20 @@
+/**
+ * @file QuickSelect2.hpp
+ * @author Nicholas Szul
+ * @brief interface for quickselect2
+ * @version 0.1
+ * @date 2024-05-06
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+#include <iostream>
+#include <vector>
+#include <string>
+#include <cmath>
+#include <iomanip>
+#include <fstream>
+#include <sstream>
+#include <algorithm>
+
+void quickSelect2(const std::string & header, std::vector<int> data);
